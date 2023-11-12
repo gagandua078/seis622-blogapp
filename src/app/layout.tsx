ab1e381a300cff7d1ee0b8cf29540c89ac105fd7
GDS K S <39922405+gagandua078@users.noreply.github.com>
2023-11-12 00:55:59 -0600

    Adding Subpages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,83 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Blogging Application',
-  description: 'NextJS blogging application',
+  metadataBase: new URL('https://glinweb.com'),
+  title:
+    'GETYSTORE BLOGGING APP - WEBANDESIGNS | GLINCKER | G-NETWORK | GDS K S | GDSKS',
+  description:
+    'The Opensource Blogging Application for user to create their own blog and share their knowledge to the world.',
+  applicationName: 'Getystore Blogging Application',
+  keywords: 'Glinweb, GDSKS, WEBANDESIGNS, GDS K S, GLINCKER, CSS Framework',
+  robots: 'all',
+  appleWebApp: {
+    title: 'Gettystore Blogging Application',
+    statusBarStyle: 'black-translucent',
+    startupImage: [
+      '/favicon/favicon.ico',
+      {
+        url: '/favicon/favicon.ico',
+        media: '(device-width: 768px) and (device-height: 1024px)',
+      },
+    ],
+  },
+  icons: {
+    icon: [
+      // Favicon
+      {
+        url: '/favicon/favicon.ico',
+        rel: 'icon',
+        href: '/favicon/favicon.ico',
+      },
+      // Apple Favicon
+      {
+        url: '/favicon/apple-touch-icon.png',
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/favicon/apple-touch-icon.png',
+      },
+      // 32 Favicon
+      {
+        url: '/favicon/favicon-32x32.png',
+        rel: 'icon',
+        sizes: '32x32',
+        href: '/favicon/favicon-32x32.png',
+      },
+      // 16 Favicon
+      {
+        url: '/favicon/favicon-16x16.png',
+        rel: 'icon',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png',
+      },
+      // mask-icon
+      {
+        url: '/favicon/safari-pinned-tab.svg',
+        rel: 'mask-icon',
+        href: '/favicon/safari-pinned-tab.svg',
+      },
+    ],
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  openGraph: {
+    images: [
+      {
+        url: 'https://cdn.glincker.com/glinweb/Glinweb.png',
+        width: 800,
+        height: 600,
+      },
+    ],
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://glinweb.com',
+    title: 'GETYSTORE BLOGGING Applicaion | GLINCKER | GDS K S',
+    description:
+      'The Opensource Blogging Application for user to create their own blog and share their knowledge to the world.',
+    siteName: 'Glinweb',
+  },
 };
 
 export default function RootLayout({
