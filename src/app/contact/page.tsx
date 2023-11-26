350c27f5406e7eb5fc6d0e588f023dff1350d0ff
kstern00 <Kristina Stern>
2023-11-26 14:53:33 -0600

    contacts commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return <
+  <div className="contactPage">
+    <div className="container"style={{ paddingTop: '60px', paddingBottom: '60px' }}>
+        <div className="col-md-6">
+          <Image
+            src={contactImage}
+            className="contact-us"
+            alt="image1"
+          />
+        </div>
+        <h1>Email Us!</h1>
+        <p><a href="mailto:webmaster@example.com">Gagan Deep Singh</a></p>
+        <p><a href="mailto:webmaster@example.com">Kristina Stern</a></p>
+        <p><a href="mailto:webmaster@example.com">Jamaal Mohamud</a></p>
+      </div>
+    </div> >
+    >
+
 };
 
-export default Contact;
+export default Contact {
+
+};
