797cceee19c7b4aec5d413e976d16ffab5df0f60
kstern00 <Kristina Stern>
2023-11-26 15:03:31 -0600

    contacts commit

---
+++ src/app/contact/components/banner.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Image from '../../public/assets/contact';
+
+const banner = () => {
+  return (
+    <>
+  <div className="contactPage">
+    <div className="container"style={{ paddingTop: '60px', paddingBottom: '60px' }}>
+        <div className="col-md-6">
+          <Image
+            src={website-page-contact-us-email-600nw-1958537320}
+            className="contact-us"
+            alt="image1"
+          />
+        </div>
+      </div>
+    </div>>
+    </>
+  );
+};
+
+export default banner;
---
+++ src/app/contact/components/info.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const info = () => {
+  return (
+    <>
+  <div className="contactPage">
+    <div className="container"style={{ paddingTop: '60px', paddingBottom: '60px' }}>
+    <h1>Email Us!</h1>
+        <p><a href="mailto:webmaster@example.com">Gagan Deep Singh</a></p>
+        <p><a href="mailto:webmaster@example.com">Kristina Stern</a></p>
+        <p><a href="mailto:webmaster@example.com">Jamaal Mohamud</a></p>
+
+      </div>
+    </div>>
+    </>
+  );
+};
+
+export default info;
